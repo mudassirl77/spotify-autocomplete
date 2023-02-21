c45443169fa8acc1f556f22dbdb860dfefc9c92b
mudassirl77 <mudassir.lallmahamood@gmail.com>
2023-02-22 01:11:44 +0400

    Add schema to suppress warning

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/ui/navbar/navbar.component';
 import { GridHeaderComponent } from './components/ui/grid-header/grid-header.component';
@@ -21,6 +21,7 @@ import { InputSearchComponent } from './components/ui/input-search/input-search.
     NavbarComponent,
     GridHeaderComponent,
     IconComponent
-  ]
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class SharedModule { }
